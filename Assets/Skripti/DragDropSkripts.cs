24b4086696cc8083bb10a87b105243d23e2e1472
Karlis Zeiburlins <karliszeibis@gmail.com>
2021-04-06 11:46:13 +0300

    uzsāk pirma skripta izveidi

---
+++ Assets/Skripti/DragDropSkripts.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//jāimporte lai varetu stradata ar eventsystems
+using UnityEngine.EventSystems;
+public class DragDropSkripts : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler /* IEndDragHandler*/ {
+    //uzglabas noradi uzz objektu skriptu
+    //  public Objekti objektuSkripts;
+    //velakamam objektam piestiprinata komponente
+    private CanvasGroup kanvasGrupa;
+    //vilktāobjekta atrašanās vietas kooardinātu maiņa
+    private RectTransform velkObjRectTransf;
+
+    private void Awake()
+    {
+        //piekļūst objektam piesastītajai CanvasGroup komponentei
+        kanvasGrupa = GetComponent<CanvasGroup>();
+        //Piekļūst objektam RectTransform komponentei
+        velkObjRectTransf = GetComponent<RectTransform>();
+    }
+
+    //nostrādā nospiežot peles klikšķi uz objektu
+    public void OnPointerDown(PointerEventData notikums)
+    {
+        Debug.Log("Uzklikšķina uz velkama objekta!");
+    }
+
+    //nostrada uzsākot vilksanu 
+    public void OnBeginDrag(PointerEventData notikums)
+    {
+        Debug.Log("Uzsākta vilkšana!");
+        //attīra pēdejo vilkto objektu 
+        //objektuSkripts.pedejaisVilktais = null;
+        //uzsākot vilkt objektu tas paliek nedaudz caurspidigs
+        kanvasGrupa.alpha = 0.6f;
+        //lai obejktam veretu iet cauri rycast stars
+        kanvasGrupa.blocksRaycasts = false;
+
+    }
+
+    //nostrada reli notiekot vilkšanai
+    public void OnDrag(PointerEventData notikums)
+    {
+        Debug.Log("Notiek vilkšana!");
+        //velkObjRectTransf.anchoredPosition += notikums.delta / objektuSkripts.kanva.scaleFactor;
+    }
+}
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # VelcUnNomet
 ## Veicamo darbu saraksts:
 - [x] izveidot github repo
-- [ ] sagatavo unity darba uzsakšanai
+- [x] sagatavo unity darba uzsakšanai
 - [ ] realizē objektu vilkšanu un oorientēšanu (uz kartes, nepareizi, pareizi)
 - [ ] realizē objektu transformācijas (izmera maiņa)
 - [ ] realizē laika atskaiti un uzvaras logu (laiks, zvaigznītes)
